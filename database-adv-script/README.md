30c261d452d382273eb5d473117ffda9766cbdea
Kenneth Imade <kenneth.imade@yahoo.com>
2025-10-27 20:13:57 +0000

    feat: added subqueries

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -1,7 +1,14 @@
-# Join Queries
+# Advanced Queries
 
 This script contains SQL queries that retrieves the following information:
+
+## Join Queries
 
 - All bookings and the respective users who made those bookings.
 - All properties and their reviews, inclucing properties that have no reviews.
 - All users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+## Subqueries (correlated and non-correlated)
+
+- All properties where the average rating is greater than 4.0 using a subquery.
+- Find users who have made more than 3 bookings.
---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,35 @@
+-- ======================================================
+-- Airbnb SubQueries
+-- Author: Kenneth Imade
+-- Date: 2025-10-27
+-- Description: This script retrieves the following data
+--  points using queries.
+--   1. All properties where the average rating is greater than 4.0.
+--   2. Users who have made more than 3 bookings.
+-- ======================================================
+
+
+-- All properties where the average rating is greater than 4.0
+SELECT
+    r.property_id,
+    AVG(r.rating) as average_rating
+FROM Review r
+WHERE r.property_id IN (SELECT property_id FROM Property)
+GROUP BY r.property_id
+HAVING average_rating > 4
+ORDER BY average_rating DESC;
+
+-- Users who have made more than 3 bookings
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    u.phone_number
+FROM User u
+WHERE (
+    SELECT
+        COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
